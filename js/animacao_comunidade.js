cb3b820b2e849575a1af74b80eee91a22ab8e849
Paulo Henrique <paulo253hgg@gmail.com>
2026-06-02 20:29:45 -0300

    Feat: Numeros crescendo

---
+++ js/animacao_comunidade.js
@@ -0,0 +1,40 @@
+const contadores = document.querySelectorAll(".comunidade__numeros__area__caixa__numero");
+
+//Cria um observador para checar QUANDO o elemento entra na tela
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach(entry =>{
+        if (entry.isIntersecting){
+            animarContador(entry.target);
+            observer.unobserve(entry.target); // Pra executar uma vez só
+        }
+    });
+});
+
+contadores.forEach(contador =>{
+    observer.observe(contador);
+})
+
+//função pra mudar o valor
+function animarContador(elem){
+    console.log("entrou", elem)
+    const valorAlvo = Number(elem.dataset.valor);
+    const duracao = 1500; //em ms
+    const inicio = performance.now(); //quando comecou
+
+    //declara dentro pra evitar comflito
+    function atualizar(tempoAtual){
+        
+        const progresso = Math.min((tempoAtual - inicio) / duracao, 1);
+        console.log("atualizou", progresso)
+        console.log(elem.textContent)
+        elem.textContent = (Math.floor(progresso * valorAlvo)).toLocaleString("pt-BR"); //arredondar
+
+        if (progresso < 1){
+            requestAnimationFrame(atualizar);
+        } else{
+            elem.textContent = valorAlvo.toLocaleString("pt-BR");
+        }
+    }
+
+    requestAnimationFrame(atualizar);
+}
