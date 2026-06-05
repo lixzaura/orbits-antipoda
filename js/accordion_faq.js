b2bc1d5db24ff5195b99f731a6732273647cb70b
Paulo Henrique <paulo253hgg@gmail.com>
2026-06-05 12:56:47 -0300

    Feat: accordion faq

---
+++ js/accordion_faq.js
@@ -0,0 +1,15 @@
+const cards = document.querySelectorAll(".faq__perguntas__card");
+
+cards.forEach(card =>{
+    const question = card.querySelector(".faq__perguntas__card__pergunta");
+    const answer = card.querySelector(".faq__perguntas__card__resposta")
+    
+    question.classList.add("inativo")
+    answer.classList.add("inativo");
+
+    question.addEventListener("click", () =>{
+        answer.classList.toggle("inativo");
+        question.classList.toggle("inativo");
+    });
+
+});
