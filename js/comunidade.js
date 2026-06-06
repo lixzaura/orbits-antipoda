281dbf30c1484fb1759ae2f7d5547c7b32003b47
Paulo Henrique <paulo253hgg@gmail.com>
2026-06-06 17:55:27 -0300

    Feat: js comunidade form

--- js/comunidade.js
+++ js/comunidade.js
@@ -1,6 +1,52 @@
 const contadores = document.querySelectorAll(".comunidade__numeros__area__caixa__numero");
 
-const modal
+const emails = [];
+
+const modal = document.querySelector(".comunidade__iniciativas__modal");
+const modalInput = document.querySelector(".comunidade__iniciativas__formulario__input");
+const modalLabel = document.querySelector(".comunidade__iniciativas__formulario__label");
+const modalForm = document.querySelector(".comunidade__iniciativas__modal__formulario");
+const modalButton = modal.querySelector("button");
+const modalFechar = modal.querySelector(".comunidade__iniciativas__modal__fechar");
+const modalStatus = modal.querySelector(".comunidade__iniciativas__modal__status")
+const iniciativaBotoes = document.querySelectorAll(".comunidade__iniciativas__iniciativa__caixa__botao");
+
+modal.classList.add("inativo");
+modalStatus.classList.add("inativo");
+
+
+iniciativaBotoes.forEach(botao =>{
+    botao.addEventListener("click", () =>{
+        let ini = botao.dataset.iniciativa;
+        modal.classList.remove("inativo");
+        modalLabel.textContent = modalLabel.dataset.texto + ini;
+    });
+});
+
+function removeModal(){
+    modal.classList.add("inativo");
+}
+
+modalFechar.addEventListener("click", removeModal);
+
+modalForm.addEventListener("submit", (e) =>{
+    console.log(e.target)
+    e.preventDefault();
+    if (!modalInput.value.trim()){
+        modalStatus.classList.remove("inativo");
+        setTimeout(() =>{
+            modalStatus.classList.add("inativo");
+        }, 10000);
+    }
+
+    emails.push(modalInput.value.trim());
+    modalInput.value = "";
+    removeModal();
+});
+
+
+
+
 
 //Cria um observador para checar QUANDO o elemento entra na tela
 const observer = new IntersectionObserver((entries) =>{
